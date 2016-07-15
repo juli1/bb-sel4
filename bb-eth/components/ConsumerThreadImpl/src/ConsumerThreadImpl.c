afcde0a2b26226d8588348b20ac0dbd45a83b3cc
Julien Delange <julien@gunnm.org>
2016-07-15 16:04:38 -0400

    update with new headers

--- bb-eth/components/ConsumerThreadImpl/src/ConsumerThreadImpl.c
+++ bb-eth/components/ConsumerThreadImpl/src/ConsumerThreadImpl.c
@@ -7,8 +7,140 @@
 #include "locator.h"
 //#include "httpd.h"
 #include "lwiplib.h"
-//#include "lwipopts.h"
+#include "lwipopts.h"
 
+#include "soc_AM335x.h"
+#include "beaglebone.h"
+#include "interrupt.h"
+#include "hw_cm_per.h"
+#include "hw_cm_dpll.h"
+#include "dmtimer.h"
+
+#define CONFIG_SWITCH_EXIT_CMD             (2)
+
+static volatile unsigned int flagIsr = 1;
+
+
+void DMTimer7ModuleClkConfig(void)
+{
+    HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) =
+                             CM_PER_L3S_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) &
+     CM_PER_L3S_CLKSTCTRL_CLKTRCTRL) != CM_PER_L3S_CLKSTCTRL_CLKTRCTRL_SW_WKUP);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) =
+                             CM_PER_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) &
+     CM_PER_L3_CLKSTCTRL_CLKTRCTRL) != CM_PER_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_L3_INSTR_CLKCTRL) =
+                             CM_PER_L3_INSTR_CLKCTRL_MODULEMODE_ENABLE;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L3_INSTR_CLKCTRL) &
+                               CM_PER_L3_INSTR_CLKCTRL_MODULEMODE) !=
+                                   CM_PER_L3_INSTR_CLKCTRL_MODULEMODE_ENABLE);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKCTRL) =
+                             CM_PER_L3_CLKCTRL_MODULEMODE_ENABLE;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKCTRL) &
+        CM_PER_L3_CLKCTRL_MODULEMODE) != CM_PER_L3_CLKCTRL_MODULEMODE_ENABLE);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) =
+                             CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) &
+                              CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL) !=
+                                CM_PER_OCPWP_L3_CLKSTCTRL_CLKTRCTRL_SW_WKUP);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_L4LS_CLKSTCTRL) =
+                             CM_PER_L4LS_CLKSTCTRL_CLKTRCTRL_SW_WKUP;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L4LS_CLKSTCTRL) &
+                             CM_PER_L4LS_CLKSTCTRL_CLKTRCTRL) !=
+                               CM_PER_L4LS_CLKSTCTRL_CLKTRCTRL_SW_WKUP);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_L4LS_CLKCTRL) =
+                             CM_PER_L4LS_CLKCTRL_MODULEMODE_ENABLE;
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_L4LS_CLKCTRL) &
+      CM_PER_L4LS_CLKCTRL_MODULEMODE) != CM_PER_L4LS_CLKCTRL_MODULEMODE_ENABLE);
+
+    /* Select the clock source for the Timer7 instance. */
+    HWREG(SOC_CM_DPLL_REGS + CM_DPLL_CLKSEL_TIMER7_CLK) &=
+          ~(CM_DPLL_CLKSEL_TIMER7_CLK_CLKSEL);
+
+    HWREG(SOC_CM_DPLL_REGS + CM_DPLL_CLKSEL_TIMER7_CLK) |=
+          CM_DPLL_CLKSEL_TIMER7_CLK_CLKSEL_CLK_M_OSC;
+
+    while((HWREG(SOC_CM_DPLL_REGS + CM_DPLL_CLKSEL_TIMER7_CLK) &
+           CM_DPLL_CLKSEL_TIMER7_CLK_CLKSEL) !=
+           CM_DPLL_CLKSEL_TIMER7_CLK_CLKSEL_CLK_M_OSC);
+
+    HWREG(SOC_CM_PER_REGS + CM_PER_TIMER7_CLKCTRL) |=
+                             CM_PER_TIMER7_CLKCTRL_MODULEMODE_ENABLE;
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_TIMER7_CLKCTRL) &
+     CM_PER_TIMER7_CLKCTRL_MODULEMODE) != CM_PER_TIMER7_CLKCTRL_MODULEMODE_ENABLE);
+
+    while((HWREG(SOC_CM_PER_REGS + CM_PER_TIMER7_CLKCTRL) & 
+     CM_PER_TIMER7_CLKCTRL_IDLEST) != CM_PER_TIMER7_CLKCTRL_IDLEST_FUNC);
+
+    while(!(HWREG(SOC_CM_PER_REGS + CM_PER_L3S_CLKSTCTRL) &
+            CM_PER_L3S_CLKSTCTRL_CLKACTIVITY_L3S_GCLK));
+
+    while(!(HWREG(SOC_CM_PER_REGS + CM_PER_L3_CLKSTCTRL) &
+            CM_PER_L3_CLKSTCTRL_CLKACTIVITY_L3_GCLK));
+
+    while(!(HWREG(SOC_CM_PER_REGS + CM_PER_OCPWP_L3_CLKSTCTRL) &
+           (CM_PER_OCPWP_L3_CLKSTCTRL_CLKACTIVITY_OCPWP_L3_GCLK |
+            CM_PER_OCPWP_L3_CLKSTCTRL_CLKACTIVITY_OCPWP_L4_GCLK)));
+
+    while(!(HWREG(SOC_CM_PER_REGS + CM_PER_L4LS_CLKSTCTRL) &
+           (CM_PER_L4LS_CLKSTCTRL_CLKACTIVITY_L4LS_GCLK |
+            CM_PER_L4LS_CLKSTCTRL_CLKACTIVITY_TIMER7_GCLK)));
+}
+
+static void DMTimerIsr(void)
+{
+    /* Clear the status of the interrupt flags */
+    DMTimerIntStatusClear(SOC_DMTIMER_7_REGS, DMTIMER_INT_OVF_EN_FLAG);
+
+    DMTimerDisable(SOC_DMTIMER_7_REGS);
+
+    flagIsr = TRUE;
+}
+
+
+
+void DelayTimerSetup(void)
+{   
+
+#ifdef DELAY_USE_INTERRUPTS
+    /* This function will enable clocks for the DMTimer7 instance */
+    DMTimer7ModuleClkConfig();
+
+    /* Registering DMTimerIsr */
+    IntRegister(SYS_INT_TINT7, DMTimerIsr);
+
+    /* Set the priority */
+    IntPrioritySet(SYS_INT_TINT7, 0, AINTC_HOSTINT_ROUTE_IRQ);
+
+    /* Enable the system interrupt */
+    IntSystemEnable(SYS_INT_TINT7);
+
+    DMTimerCounterSet(SOC_DMTIMER_7_REGS, 0);
+
+    /* Configure the DMTimer for Auto-reload and compare mode */
+    DMTimerModeConfigure(SOC_DMTIMER_7_REGS, DMTIMER_ONESHOT_NOCMP_ENABLE);
+#else
+    DMTimer7ModuleClkConfig();
+
+    DMTimerModeConfigure(SOC_DMTIMER_7_REGS, DMTIMER_ONESHOT_NOCMP_ENABLE);
+#endif
+
+}
 
 
 
@@ -29,10 +161,10 @@ int run(void)
 
    unsigned int ipAddr;
    unsigned int initFlg = 1;
-//   LWIP_IF lwipIfPort1, lwipIfPort2;
+   LWIP_IF lwipIfPort1, lwipIfPort2;
 
 #ifdef LWIP_CACHE_ENABLED
-   CacheEnable(CACHE_ALL);
+//   CacheEnable(CACHE_ALL);
 #endif
 
 //   CPSWPinMuxSetup();
@@ -102,7 +234,7 @@ int run(void)
       {
          printf("\n\r\n\r === CPSW Configurations === ");
          printf("\n\r\n\r === Available PHY Configurations ===");
-         pintf("\n\r\n\r1 - Configure Phy of a Port ");
+         printf("\n\r\n\r1 - Configure Phy of a Port ");
          printf("\n\r\n\r2 - Exit ");
          initFlg = 0;
       }
